8fcaf6e782d18252da83c7d8ee28107961211448
EduardoFrs <fernandeseduardo00@gmail.com>
2024-04-25 16:48:02 +0200

    change error statement

--- executor.c
+++ executor.c
@@ -85,7 +85,8 @@ void execute_command(char **parsed_command, char *program_name, char **env)
 
 	if (!cmd_path)
 	{
-		fprintf(stderr, "%s: No such file or directory %s\n", program_name, parsed_command[0]);
+		fprintf(stderr, "%s: ", program_name);
+		perror(parsed_command[0]);
 		return;
 	}
 
