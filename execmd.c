0a78738588348f8ffcea802313e114ef72c1d758
EduardoFrs <fernandeseduardo00@gmail.com>
2024-04-25 10:49:27 +0200

    betty files

--- execmd.c
+++ execmd.c
@@ -1,6 +1,7 @@
 #include "shell.h"
 
-void execmd(char **argv){
+void execmd(char **argv)
+{
 	char *command = NULL, *actual_command = NULL;
 	pid_t pid = fork();
 	int status;
@@ -15,13 +16,10 @@ void execmd(char **argv){
 	{
 		if (argv)
 		{
-			/* get the command */
 			command = argv[0];
 
-			/* generate the path to this command before passing it to execve */
 			actual_command = get_location(command);
 
-			/* execute the actual command with execve */
 			if (execve(actual_command, argv, NULL) == -1)
 			{
 				perror("Error:");
@@ -31,7 +29,7 @@ void execmd(char **argv){
 
 	else
 	{
-		wait (&status);
+		wait(&status);
 
 		if (WIFEXITED(status))
 		{
--- main.c
+++ main.c
@@ -11,32 +11,25 @@ int main(int ac, char **argv)
 	char *token;
 	int i;
 
-	/* declaring void variables */
 	(void)ac;
 
-	/* Create a loop for the shell's prompt */
 	while (1)
 	{
 		printf("%s", prompt);
 		nchars_read = getline(&lineptr, &n, stdin);
-		/* check if the getline function failed or reached EOF or user use CTRL + D */
 		if (nchars_read == -1)
 		{
 			return (-1);
 		}
 
-		/* allocate space for a copy of the lineptr */
 		lineptr_copy = malloc(sizeof(char) * nchars_read);
 		if (lineptr_copy == NULL)
 		{
 			perror("tsh: memory allocation error");
 			return (-1);
 		}
-		/* copy lineptr to lineptr_copy */
 		strcpy(lineptr_copy, lineptr);
 
-		/********** split the string (lineptr) into an array of words ********/
-		/* calculate the total number of tokens */
 		token = strtok(lineptr, delim);
 
 		while (token != NULL)
@@ -46,10 +39,8 @@ int main(int ac, char **argv)
 		}
 		num_tokens++;
 
-		/* Allocate space to hold the array of strings */
 		argv = malloc(sizeof(char *) * num_tokens);
 
-		/* Store each token in the argv array */
 		token = strtok(lineptr_copy, delim);
 
 		for (i = 0; token != NULL; i++)
@@ -61,7 +52,6 @@ int main(int ac, char **argv)
 		}
 		argv[i] = NULL;
 
-		/* check if arg is "exit"*/
 		if (argv[0] != NULL && strcmp(argv[0], "exit") == 0)
 		{
 			break;
@@ -78,14 +68,8 @@ int main(int ac, char **argv)
 			}
 			continue;
 		}
-
-		/* execute the command */
 		execmd(argv);
-
 	}
-
-
-	/* free up allocated memory */
 	free(lineptr_copy);
 	free(lineptr);
 
