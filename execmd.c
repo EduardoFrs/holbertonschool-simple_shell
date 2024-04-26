06a5db0532cf03da76de2f0a86f533473dd362f7
EduardoFrs <fernandeseduardo00@gmail.com>
2024-04-26 10:14:33 +0200

    return to our version

---
+++ execmd.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * execmd - function to execute a command
+ * @argv: An array of arguments
+ * Description: forks a new process and executes a command
+ *
+ */
+
+void execmd(char **argv)
+{
+	char *command = NULL, *actual_command = NULL;
+	pid_t pid = fork();
+	int status;
+
+	if (pid == -1)
+	{
+		perror("Error:");
+		free(argv);
+	}
+
+	if (pid == 0)
+	{
+		if (argv)
+		{
+			command = argv[0];
+
+			actual_command = get_location(command);
+
+			if (execve(actual_command, argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+		}
+	}
+
+	else
+	{
+		wait(&status);
+
+		if (WIFEXITED(status))
+		{
+			if (WEXITSTATUS(status) != 0)
+			{
+				free(argv);
+				exit(1);
+			}
+		}
+
+	}
+}
--- main.c
+++ main.c
@@ -1,33 +1,87 @@
 #include "shell.h"
 
 /**
- * main - entry point for our simple shell program, checks for interactive
- *  or non-interactive
- * @argc: count of command line arguments
- * @argv: command line arguments array
- * Return: 0 if successful exit
+ * main - Entry point for the shell program
+ * @ac: Number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: If an error occurs, return -1. Otherwise 0
  */
 
-int main(int argc, char **argv)
+int main(int ac, char **argv)
 {
-	char *input_line;
-	char *newline = "\n";
-	(void)argc;
+    char *prompt = "(Shell) $ ";
+	char *lineptr = NULL, *lineptr_copy = NULL;
+	size_t n = 0;
+	ssize_t nchars_read;
+	const char *delim = " \n";
+	int num_tokens = 0;
+	char *token;
+	int i;
+
+	(void)ac;
 
 	while (1)
 	{
-		prompt_and_read_input(&input_line);
-		if (!input_line)
+		printf("%s", prompt);
+		nchars_read = getline(&lineptr, &n, stdin);
+		if (nchars_read == -1)
 		{
-			if (isatty(STDIN_FILENO))
-			{
-				write(STDOUT_FILENO, newline, strlen(newline));
-			}
+            perror("Error reading input");
+            free(lineptr);
+			return (-1);
+		}
+
+		lineptr_copy = malloc(sizeof(char) * nchars_read);
+		if (lineptr_copy == NULL)
+		{
+			perror("tsh: memory allocation error");
+			return (-1);
+		}
+		strcpy(lineptr_copy, lineptr);
+
+		token = strtok(lineptr, delim);
+
+		while (token != NULL)
+		{
+			num_tokens++;
+			token = strtok(NULL, delim);
+		}
+		num_tokens++;
+
+		argv = malloc(sizeof(char *) * num_tokens);
+
+		token = strtok(lineptr_copy, delim);
+
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+
+		if (argv[0] != NULL && strcmp(argv[0], "exit") == 0)
+		{
 			break;
 		}
-		process_command(input_line, argv, environ);
-		free(input_line);
+
+		if (argv[0] != NULL && strcmp(argv[0], "env") == 0)
+		{
+			char **env = environ;
+
+			while (*env)
+			{
+				printf("%s\n", *env);
+				env++;
+			}
+			continue;
+		}
+		execmd(argv);
 	}
+	free(lineptr_copy);
+	free(lineptr);
 
 	return (0);
 }
